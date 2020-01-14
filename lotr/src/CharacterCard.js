362ad1d9baef20955a35a110de97da6174515f89
Robbie Porter <rthomporter@gmail.com>
2020-01-13 18:16:00 -0600

    final

--- lotr/src/CharacterCard.js
+++ lotr/src/CharacterCard.js
@@ -1,12 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 const CharacterCard = props => {
+  const handleDelete = id => {
+    axios
+      .delete(`http://localhost:5000/api/users/${id}`)
+      .then(res => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch(err => console.log(err.message));
+  };
+
   return (
     <div>
       <h1>{props.data.name}</h1>
       <p>bio: {props.data.bio}</p>
       <button>edit</button>
-      <button>delete</button>
+      <button onClick={() => handleDelete(props.data.id)}>delete</button>
     </div>
   );
 };
